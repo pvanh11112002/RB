e785f6ac68e5b91a04ca229881390d9499b4799b
Vanh <phamvietanh.study@gmail.com>
2025-04-26 00:31:35 +0700

    15

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    int currentScene = 0;
+    [SerializeField] float delayTimeBeforeLoadSequence = 2f;
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
     private void OnCollisionEnter(Collision other)
     {
         switch(other.gameObject.tag)
@@ -10,15 +17,39 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log(1);
                 break;
             case "Finish":
-                Debug.Log(2);
+                StartFinishSequence();
                 break;
             case "Fuel":
                 Debug.Log(3);
                 break;
             default:
-                Debug.Log(4);
+                StartCrashSequence();
                 break;
 
         }    
+    }
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(currentScene);
+    }
+    private void LoadNextLevel()
+    {
+        
+        currentScene++;
+        if (currentScene >= SceneManager.sceneCountInBuildSettings)
+        {
+            currentScene = 0;
+        }
+        SceneManager.LoadScene(currentScene);
+    }
+    private void StartCrashSequence()
+    {
+        GetComponent<PlayerMovementHandle>().enabled = false;
+        Invoke("ReloadLevel", delayTimeBeforeLoadSequence);
+    }
+    private void StartFinishSequence()
+    {
+        GetComponent<PlayerMovementHandle>().enabled = false;
+        Invoke("LoadNextLevel", delayTimeBeforeLoadSequence);
     }
 }
